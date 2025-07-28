469ec7bf78db9c7eb99eb209a7941313b3569277
riyaverma85 <riyaverma2976@gmail.com>
2025-07-28 01:27:00 -0700

    bodyparser

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 
 const express=require("express");
 const app=express();
-
 const stuRoute=require("./Routes/stuRoutes");
+const bodyParser = require('body-parser')
 app.set("view engine","ejs");
 app.use("/", stuRoute);
 app.use(express.static("./public"));
 
+//Body parser middleware
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json())
 
 app.listen(4000,()=>{
     console.log("server is running on port 4000")
