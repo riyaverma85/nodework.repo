5cc1354e86dd5ddc757e008a64e1792d01c6315b
riyaverma85 <riyaverma2976@gmail.com>
2025-07-28 00:45:10 -0700

    style

--- Routes/stuRoutes.js
+++ Routes/stuRoutes.js
@@ -4,6 +4,7 @@ const route=express.Router();
 const stuController=require("../controllers/stuController");
 
 route.get("/",stuController.homepage)
+route.get("/insert",stuController.insertpage)
 
 
 module.exports=route;
